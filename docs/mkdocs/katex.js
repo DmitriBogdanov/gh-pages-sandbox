029a52cbd569da431612cf82db7efa066313a2d7
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2025-08-29 19:40:23 +0300

    Added support for code blocks nested in content blocks. Adjusted logos, colors, footer and etc. Added a fix for missing callout style.

--- docs/mkdocs/katex.js
+++ docs/mkdocs/katex.js
@@ -1,10 +1,17 @@
-document$.subscribe(({ body }) => { 
-  renderMathInElement(body, {
-    delimiters: [
-      { left: "$$",  right: "$$",  display: true },
-      { left: "$",   right: "$",   display: false },
-      { left: "\\(", right: "\\)", display: false },
-      { left: "\\[", right: "\\]", display: true }
-    ],
-  })
+// __________________________________ CONTENTS ___________________________________
+// 
+//    This is a KaTeX setup for rendering LaTeX formulas in documentation.
+//    
+//    See: https://squidfunk.github.io/mkdocs-material/reference/math/
+// _______________________________________________________________________________
+
+document$.subscribe(({ body }) => {
+    renderMathInElement(body, {
+        delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+            { left: "\\(", right: "\\)", display: false },
+            { left: "\\[", right: "\\]", display: true }
+        ],
+    })
 })
